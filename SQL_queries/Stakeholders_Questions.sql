ba97c623eae49f9103a93d822c297ca79c1ff2d4
harika16-mangu <125753014+harika16-mangu@users.noreply.github.com>
2025-02-13 16:54:22 -0500

    Stakeholders_Questions.sql

--- SQL_queries/Stakeholders_Questions.sql
+++ SQL_queries/Stakeholders_Questions.sql
@@ -1,34 +1,34 @@
 --Question 1: What are the top 5 brands by receipts scanned for most recent month?
 --Query:
-with cte1 as (
+with brands as (
 select distinct b.brandcode,b.name,b.cpg_id from analytics.public.brands b
 ), 
-cte2 as (
+receipt as (
 select distinct r.receipt_id,i.rewardsproductpartnerid,r.datescanned from analytics.public.receipts r join analytics.public.receipt_items i on r.receipt_id=i.receipt_id
 where MONTH(r.datescanned)=3
 ),
-cte3 as(
-select distinct c.name,count(d.receipt_id) as total_receipts,dense_rank() over(order by count(d.receipt_id)desc) as brand_rankings from cte1 as c join cte2 as d on c.cpg_id=d.rewardsproductpartnerid
+top_brands as(
+select distinct c.name,count(d.receipt_id) as total_receipts,dense_rank() over(order by count(d.receipt_id)desc) as brand_rankings from brands as c join receipt as d on c.cpg_id=d.rewardsproductpartnerid
 group by c.name
 )
-select e.name,e.total_receipts,brand_rankings from cte3 as e
+select e.name,e.total_receipts,brand_rankings from top_brands as e
 where e.brand_rankings<6;
 
 --Question 2: When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
 --Query:
-with cte1 as(
+with receipt1 as(
 select round(avg(totalspent),2) as average,rewardsreceiptstatus from analytics.public.receipts
 group by rewardsreceiptstatus
 having rewardsreceiptstatus='FINISHED'
 ),
-cte2 as(
+receipt2 as(
 select round(avg(totalspent),2)as average,rewardsreceiptstatus from analytics.public.receipts
 group by rewardsreceiptstatus
 having rewardsreceiptstatus='REJECTED'
 )
-select rewardsreceiptstatus,average from cte1
+select rewardsreceiptstatus,average from receipt1
 union all
-select rewardsreceiptstatus,average from cte2
+select rewardsreceiptstatus,average from receipt2
 
 --Question 3: When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
 --Query:
@@ -48,12 +48,12 @@ limit 5;
 
 --Which brand has the most transactions among users who were created within the past 6 months?
 --Query:
-with cte1 as (select distinct u.user_id,sum(r.totalspent)as total_Spent,count(distinct r.receipt_id) as total_receipts from receipts r join users u on r.userid=u.user_id
+with user1 as (select distinct u.user_id,sum(r.totalspent)as total_Spent,count(distinct r.receipt_id) as total_receipts from receipts r join users u on r.userid=u.user_id
 where month(r.createdate) between 1 and 6
 group by u.user_id
 ),
-cte2 as(select distinct b.name from brands b)
+brand1 as(select distinct b.name from brands b)
 select h.name,r.total_receipts,r.total_Spent
-from cte1 r join cte2 h
+from user1 r join brand1 h
 order by r.total_receipts desc
 limit 5;
